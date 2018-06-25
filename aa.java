e4574e0cc6979089dc7848e1dfdd043b068586b3
TakafumiMiki <karugami02468@gmail.com>
2018-06-26 00:25:46 +0900

    init lego prog

---
+++ aa.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/**
+ * aa
+ */
+public class aa {
+    
+    public static void main(String[] args) {
+        List<Integer> alpha = new ArrayList<Integer>(Arrays.asList(12,20,30));
+        int i = 20;
+        boolean set_flag = true;
+        if(alpha.contains(i)&& set_flag){
+            System.out.println("aaaa");
+        }
+        else{
+            System.out.println("naidesu");
+        } 
+    }
+}    
